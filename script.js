cc85af1b26dc01ad27ba424d6670b9145797658d
Antonio-Vo <34730@ocvts.org>
2025-01-10 14:30:00 +0000

    working lightbox

--- script.js
+++ script.js
@@ -13,34 +13,34 @@ images.forEach((Image, index) => {
         lightbox.classList.add('visible')
     });
 });
-// //close lightbox when clicking outside the content
-// lightbox.addEventListener('click', (event) => {
-//     if (event.target === lightbox) {
-//     lightbox.classList.remove('visible')
-//     }
-// });
+//close lightbox when clicking outside the content
+lightbox.addEventListener('click', (event) => {
+    if (event.target === lightbox) {
+    lightbox.classList.remove('visible')
+    }
+ });
 
-// // show next image in the light box
-// document.getElementById('next').addEventListener('click', () => {
-//     currentIndex = (currentIndex + 1) % galleryImages.length;
-//     lightboxImg.src = images[currentIndex].src
-// });
+ // show next image in the light box
+ document.getElementById('next').addEventListener('click', () => {
+     currentIndex = (currentIndex + 1) % galleryImages.length;
+    lightboxImg.src = images[currentIndex].src
+ });
 
-// // show previous images in the lightbox
-// document.getElementById('prev').addEventListener('click', () => {
-//     currentIndex = (currentIndex - 1 + galleryImages.length) % galleryImages.length;
-//     lightboxImg.src = images[currentIndex].src;    
-// })
+ // show previous images in the lightbox
+ document.getElementById('prev').addEventListener('click', () => {
+     currentIndex = (currentIndex - 1 + galleryImages.length) % galleryImages.length;
+    lightboxImg.src = images[currentIndex].src;    
+ })
 
-// // keyboard navigation for the lightbox
-// document.addEventListener('keydown' , (event) => {
-//     if (event.key === 'arrowRight') {
-//         currentIndex = (currentIndex + 1) % images.length;
-//         lightboxImg.src =images[currentIndex].src;
-//     } else if (event.key === 'ArrowRight') {
-//         currentIndex = (currentIndex - 1 + images.length) % images.length;
-//         lightboxImg.src = images[currentIndex].src;
-//     } else if (event.key === 'Escape'){
-//         lightbox.classList.remove('visible')
-//     }
-// });
+ // keyboard navigation for the lightbox
+ document.addEventListener('keydown' , (event) => {
+    if (event.key === 'arrowRight') {
+        currentIndex = (currentIndex + 1) % images.length;
+       lightboxImg.src =images[currentIndex].src;
+    } else if (event.key === 'ArrowRight') {
+currentIndex = (currentIndex - 1 + images.length) % images.length;
+        lightboxImg.src = images[currentIndex].src;
+    } else if (event.key === 'Escape'){
+       lightbox.classList.remove('visible')
+     }
+ });
